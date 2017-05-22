df7108e60fb42a8ac984c5225820d95fcd410e24
Renan Sigolo <renan.sigolo@gmail.com>
2017-05-22 01:11:01 -0300

    javascript modificado com seletor das imagens das escolas

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-// var seletor = document.querySelector('.escolas');
-// var paragrafo = document.querySelector('.resultado p');
-//
-// console.log(seletor);
-// console.log(paragrafo);
-//
-//
-//
-// seletor.addEventListener('change', calcularOrcamento);
+var imagensEscolas = document.querySelectorAll('.imagensEscolas');
+var bandeirantes = document.querySelector('#bandeirantes');
+var objetivo = document.querySelector('#objetivo');
+var marista = document.querySelector('#marista');
+var escola01 = document.querySelector('.escola01');
+var escola02 = document.querySelector('.escola02');
+var escola03 = document.querySelector('.escola03');
+
+function adicionarClasse() {
+
+  if (bandeirantes.classList.contains('active') || objetivo.classList.contains('active') || marista.classList.contains('active')) {
+    objetivo.classList.remove('active');
+    marista.classList.remove('active');
+    bandeirantes.classList.remove('active');
+  }
+    this.classList.add('active');
+}
+
+bandeirantes.addEventListener('click', adicionarClasse);
+objetivo.addEventListener('click', adicionarClasse);
+marista.addEventListener('click', adicionarClasse);
