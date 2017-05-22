053061453f8f2c9b1990b017c7802a27d2d1f43a
Renan Sigolo <renan.sigolo@gmail.com>
2017-05-22 01:17:54 -0300

    js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var imagensEscolas = document.querySelectorAll('.imagensEscolas');
 var bandeirantes = document.querySelector('#bandeirantes');
 var objetivo = document.querySelector('#objetivo');
@@ -19,3 +20,31 @@ function adicionarClasse() {
 bandeirantes.addEventListener('click', adicionarClasse);
 objetivo.addEventListener('click', adicionarClasse);
 marista.addEventListener('click', adicionarClasse);
+=======
+// var seletor = document.querySelector('.escolas');
+// var paragrafo = document.querySelector('.resultado p');
+//
+// console.log(seletor);
+// console.log(paragrafo);
+//
+//
+//
+// seletor.addEventListener('change', calcularOrcamento);
+
+adImages = ["images/banner1.jpg","images/banner2.jpg"]
+thisAd = 0
+imgCt = adImages.length
+
+function rotate() {
+  if (document.images) {
+    thisAd++
+    if (thisAd == imgCt) {
+      thisAd = 0
+    }
+    document.adBanner.src=adImages[thisAd]
+    setTimeout("rotate()", 2 * 1000)
+
+  }
+
+}
+>>>>>>> 4d10515120b91dcc793a9652e18f69a4b3368cd0
