4b9523d4995ccefc338d06a995fa78aa0ac53294
Renan Sigolo <renan.sigolo@gmail.com>
2017-09-15 03:13:00 -0300

    js linted

--- alerta.js
+++ alerta.js
@@ -1,8 +1,8 @@
 // Alerta ao clicar no botão Enviar do formulário de Contato
 let enviar = document.querySelector('#submit');
 
-alertarEnvio() => {
+let alertarEnvio = () => {
   alert('Sua mensagem foi enviada com sucesso, Obrigado !');
-}
+};
 
 enviar.addEventListener('click', alertarEnvio);
